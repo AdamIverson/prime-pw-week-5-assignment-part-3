56d41441c92f40bea5528cf0e5c69c792637007d
Adam Iverson <aciverso@gmail.com>
2021-10-20 13:08:12 -0500

    addToCollection function created

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,3 +1,16 @@
 console.log('***** Music Collection *****')
 
 let collection = [];
+
+function addToCollection(title, artist, yearPublished){
+  let album = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished
+  } //end album
+  collection.push(album);
+  return album;
+} // end addToCollection
+
+addToCollection('Feels', 'Animal Collective', '2005');
+console.log(collection);
